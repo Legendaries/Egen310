fe9d2044cf2e6a5681a3bfdeebd7aa5f0eca1cba
Legendaries <Legendaries@users.noreply.github.com>
2017-01-30 02:45:16 -0700

    Add files via upload

---
+++ egen310MP1ID2/digital/digital.ino
@@ -0,0 +1,21 @@
+//1. Reads the input state (HIGH,LOW) of the given pin
+//2. Makes a decision based on inputted logic
+//3. 
+
+int LED_PIN = 6;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Enter a 1 to turn on the LED or 0 to turn off.");
+}
+
+void loop() {
+  if(Serial.available()) {
+    if(Serial.read() == 'A') {
+      digitalWrite(LED_PIN, HIGH);
+      delay(10000);
+      digitalWrite(LED_PIN, LOW);
+    }
+  }
+}
---
+++ egen310MP1ID2/ifstatement/ifstatement.ino
@@ -0,0 +1,13 @@
+int LED_PIN = 6;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Enter a 1 to turn on the LED or 0 to turn off.");
+}
+
+void loop() {
+  if(Serial.available())
+    if(Serial.read() == '0') digitalWrite(LED_PIN, LOW);
+    else if(Serial.read() == '1') digitalWrite(LED_PIN, HIGH);
+}
