c7adfa0f9249e698afe6d57d08484905d1f20e3f
Legendaries <Legendaries@users.noreply.github.com>
2017-01-30 22:39:25 -0700

    Update loop.ino

--- egen310MP1ID2/loop.ino
+++ egen310MP1ID2/loop.ino
@@ -1,1 +1,14 @@
+int LED_PIN = 6;
 
+void setup (){
+ pinMode(LED_PIN, OUTPUT); 
+}
+
+void loop (){
+ for(int i = 0; i < 5; i++){
+  digitalWrite(LED_PIN, LOW); 
+   delay(400);
+   digitalWrite(LED_PIN, HIGH);
+   delay(400);
+ }
+}
