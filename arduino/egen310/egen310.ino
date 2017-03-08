a12924321e01ddbcd6c5d9e13dbc88fd4003e1a8
Legendaries <Legendaries@users.noreply.github.com>
2017-03-08 02:19:55 -0700

    Update egen310.ino

--- arduino/egen310/egen310.ino
+++ arduino/egen310/egen310.ino
@@ -1,33 +1,29 @@
 //Analog pins
-int SALINITY_PIN = 6;
-int PRESSURE_PIN = 7;
+int SALINITY_PIN = 0;
 
 //Digital Pins
-int RESET_PIN = 8;
 int LED_PIN = 9;
+
+//Salinity Calibration Values
 //     123
 //1%   463
 //2%   573
 //3.5% 610
+
 //ETC
-int SAFE_SALINITY = 50;
+int SAFE_SALINITY = 150;
 int DELAY = 1000;
-int PRESSURE_THRESHOLD = 50;
 
 void setup() {
-  pinMode(RESET_PIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   int salinity = analogRead(SALINITY_PIN);
-  int pressure = analogRead(PRESSURE_PIN);
-  //Send salinity and pressure to be logged on PC
-  Serial.write("S" + salinity);
-  Serial.write("P" + pressure);
+  Serial.println(salinity);
   
-  if(salinity < 20)
+  if(salinity < SAFE_SALINITY)
     digitalWrite(LED_PIN, HIGH);
   else
     digitalWrite(LED_PIN, LOW);
